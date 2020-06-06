ee38b1b3d9c76e3d037293fc0470fe1ab43316f1
kritsg <kriti.garg510@gmail.com>
2020-06-06 01:43:43 -0700

    Starting to implement add-book command

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const {prefix, token} = require('./config.json');
 // ask for the discord.js module (require it)
 const Discord = require('discord.js');
 
@@ -12,12 +12,21 @@ client.once('ready', () => {
 });
 
 // login to Discord with app's token
-client.login(config.token);
+client.login(token);
 
 // on --> this can trigger multiple times
 client.on('message', message => {
-    if(message.content == '!floop')
-    {
+    // if the code doesn't start with the required prefix, or if the author of the message is the bot, exit early
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' '); // splits the message into an array by spaces
+    const command = args.shift().toLowerCase(); // isolates the command statement 
+
+    if (command === 'add-book') {
+        if (!args.length) { // if the user didn't provide any other arguments after the command
+            return message.channel.send(`You didn't provide any arguments! Type !commands for help`);
+        }
+    } else if(command === 'floop') { // sanity check if statement
         message.channel.send('shloop');
-    }
+    } 
 });
