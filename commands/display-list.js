12f3363d0036de8b8b6720091b42eec6bfeecbd0
kritsg <kriti.garg510@gmail.com>
2020-06-21 02:12:06 -0700

    Displaying the list in an embedded message seems to be working (for now)

--- commands/display-list.js
+++ commands/display-list.js
@@ -1,21 +1,44 @@
 const Discord = require('discord.js');
+const booklib = require('./BookObject.js')
+// symbols for each status 
+var unread = '\u2610';
+var in_progress = ':orange_square:';
+var completed = '\u2611';
 
-// attempt at an embedded message based off the documentation example
-const exampleEmbed = new Discord.MessageEmbed()
+// skeleton of the embedded message
+var listEmbed = new Discord.MessageEmbed()
 	.setColor('#AECDAD')
 	.setTitle('Book List')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
 	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u2611', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+		{ name: 'uhwegweg', value: unread + ' ' + 'hihihi'})
+	// 	{ name: ':orange_square:', value: '\u2611' },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	// )
+	// .addField('Inline field title', 'Some value here', true)
+	.setTimestamp();
+	// .setFooter('Some footer text here', '../BookListPicture.JPG');
+
+updateEmbed = function(message, book_list) {
+	// not ideal, but clears all the current entries and they're added again along with new ones form the book list
+	if (book_list.length == 0) {
+		return;
+	} 
+	console.log('here');
+	listEmbed.fields = [];
+	for (const book of book_list) {
+		if (book.status === 'unread') {
+			console.log(book.fullTitle);
+			// the resulting format seems a bit iffy, need to figure out a better format later
+			listEmbed.addField(book.bookAuthor, unread + ' ' + book.bookTitle, false);
+		} else if (book.status === 'in_progress') {
+			listEmbed.addField(book.bookAuthor, in_progress + ' ' + book.bookTitle, false);
+		} else if (book.status === 'complete') {
+			listEmbed.addField(book.bookAuthor, completed + ' ' + book.bookTitle, false);
+		}
+	}	
+	message.channel.send(listEmbed);
+}
 
 
 var self = module.exports = {
@@ -23,6 +46,7 @@ var self = module.exports = {
     description: 'shows the book list using MessageEmbed',
 
 	execute: function(message, args, book_list) {
-        message.channel.send(exampleEmbed);
+		updateEmbed(message, book_list);
+        
 	}
 }
