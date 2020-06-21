9b6e1306b878960e18ab9b964aa9c5b613429b5b
kritsg <kriti.garg510@gmail.com>
2020-06-21 00:37:29 -0700

    !add-book command is currently functional

--- commands/book-list-commands.js
+++ commands/book-list-commands.js
@@ -1,38 +1,61 @@
 // This file primarily deals with modifying the contents of the book list 
-var book_list = {};
-// var book_title = '';
-// var book_author = '';
+const lib = require('./BookObject.js');
+
+var book_list = [];
+var book_title = '';
+var book_author = '';
+
+separate_title_author = function (args) {
+    let author_flag = false; // reduces the scope to just this function
+    args.forEach(element => {
+        if (element != 'by' && !author_flag) {
+            book_title += element;
+        } else if (element === 'by') {
+            author_flag = true;
+        } else if (element != 'by' && author_flag) {
+            book_author += element;
+            author_flag = false;
+        }
+    });
+    
+    // console.log(book_title);
+    // console.log(book_author);
+}
+
+// checks if book already exists in the list
+book_exists = function () {
+    for (const book of book_list) {
+        console.log('in list ' + book.bookTitle + '  ' + book.bookAuthor);
+        if (book.bookTitle === book_title && book.bookAuthor === book_author) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 var self = module.exports = {
 	name: 'book-list-commands',
     description: 'executes commands that modify the book list',
 
-    book_title: '',
-
-    book_author: '',
-
-    book_list: this.book_list,
-    
-    separate_title_author: function (args) {
-        author_flag: false;
-        args.forEach(element => {
-            if (element != 'by' && !author_flag) {
-                book_title += element;
-            } else if (element == 'by') {
-                author_flag: true;
-                continue;
-            } else if (element != 'by' && author_flag) {
-                book_author += element;
-            }
-        });
-        
-        console.log(book_author);
-        console.log(book_title);
-    },
-
     add_book: function (message, args) {
-        message.channel.send('here, supposed to be adding a book');
-        message.channel.send(`Book added: ${args}`);
+        console.log('length ' + book_list.length);
+        if (!args.length) { // if the user didn't provide any other arguments after the command
+            message.channel.send(`You didn't provide any arguments! Type !commands for command descriptions.`);
+        } else if (args[0] === ``) {
+            message.channel.send(`You didn't provide a book title! Type !commands for command descriptions.`);
+        } else {
+            separate_title_author(args);
+            console.log(book_exists());
+            if (book_exists()) {
+                message.channel.send('This book is already in your list!');
+            } else {
+                book_list.unshift(new lib.Book(book_title, book_author)); // adds the new book to the beginning
+                console.log(book_list[0].bookTitle + ' ' + book_list[0].status);
+                message.channel.send(`Book added: ${book_title} by ${book_author}`);
+            }
+            book_title = ''; book_author = ''; // resetting the variables
+        }
     }, 
 
     in_progress: function (message, args) {
